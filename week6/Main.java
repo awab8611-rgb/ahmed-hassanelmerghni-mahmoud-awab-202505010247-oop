4cd79982af155ba41bfed9bacc6b75b8ba134968
awab8611-rgb <awab8611@gmail.com>
2026-08-20 16:57:01 +0000

    week6

---
+++ week6/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Lecturer l1 = new Lecturer("L100", "Dr Ahmad",
+                "Java Programming", "Faculty of Information Technology");
+
+        l1.displayInfo();
+        l1.displaySubject();
+    }
+}
