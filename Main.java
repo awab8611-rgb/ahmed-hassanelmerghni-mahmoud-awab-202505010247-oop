3815ae0461d0da26ab6e32d19e05577d0f13e17d
awab8611-rgb <awab8611@gmail.com>
2026-08-20 19:58:12 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,10 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Lecturer l1 = new Lecturer("L100", "Dr Ahmad",
-                "Java Programming", "Faculty of Information Technology");
+        Student s1 = new Student();
 
-        l1.displayInfo();
-        l1.displaySubject();
+        s1.setStudentID("CU12345");
+        s1.setName("Ali");
+        s1.setCGPA(3.75);
+        s1.setProgramme("BIT");
+
+        System.out.println("Student ID : " + s1.getStudentID());
+        System.out.println("Name    : " + s1.getName());
+        System.out.println("CGPA    : " + s1.getCGPA());
+        System.out.println("Programme: " + s1.getProgramme());
     }
 }
